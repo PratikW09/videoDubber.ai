7f6c94a965a3d40de19f1e5a1759b0699db4837b
Pratik09W <walalepratik09@gmail.com>
2025-03-27 14:27:17 +0530

    set the project

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,14 @@
 "use client";
 
 import { Title, Container } from "@mantine/core";
-import TextInputArea from "./components/TextInputArea";
 
 export default function Home() {
   return (
     <Container size="md">
       <Title order={1} mb="lg" mt="md" fw={500} ta="center">
-        Text Highlighter with Custom Colors 🎨
+        Hellow world..!
       </Title>
-      <TextInputArea />
+      
     </Container>
   );
 }
